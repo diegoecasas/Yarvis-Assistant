1d394b9620ae7b80431328e148e94720e323f513
diegoe.casas <diegoe.casas@gmail.com>
2025-01-03 02:01:15 -0500

    Create constants.py

---
+++ config/constants.py
@@ -0,0 +1,10 @@
+# config/constants.py
+
+# Rutas importantes
+DATABASE_PATH = "database/yarvis.db"
+LOG_FILE_PATH = "logs/yarvis.log"
+
+# Mensajes predefinidos
+WELCOME_MESSAGE = "Hola, {user_name}. Soy Yarvis. Estoy listo para ayudarte."
+PROCESSING_MESSAGE = "Procesando tu solicitud, {user_name}."
+ERROR_MESSAGE = "Lo siento, {user_name}. Ocurrió un error. Intenta nuevamente."
